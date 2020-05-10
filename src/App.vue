5881de0bc0c4d49d226e3885bbf3715ab65bd3dc
jean zenzeluk <jeanzenzeluk@hotmail.com>
2020-05-10 15:55:03 -0300

    testeeee

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,56 @@
 <template>
-  <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar>
-
-    <v-content>
-      <HelloWorld/>
-    </v-content>
-  </v-app>
+    <p>teste</p>
+    <v-container fluid>
+        <v-row>
+            <v-col cols="12">
+                <v-row
+                :align="alignment"
+                :justify="justify"
+                class="grey lighten-5"
+                style="height: 300px;"
+                >
+                <v-card
+                    v-for="n in 3"
+                    :key="n"
+                    class="ma-3 pa-6"
+                    outlined
+                    tile
+                    >
+                 Column
+             </v-card>
+        </v-row>
+        </v-col>
+        <v-col cols="12">
+        <v-row justify="center">
+        <v-col
+        cols="6"
+        md="2"
+        >
+        <v-select
+        v-model="alignment"
+        :items="alignmentsAvailable"
+        label="Align"
+        ></v-select>
+        </v-col>
+        <v-col
+        cols="6"
+        md="2"
+        >
+        <v-select
+        v-model="justify"
+        :items="justifyAvailable"
+        label="Justify"
+        ></v-select>
+        </v-col>
+        </v-row>
+        </v-col>
+        </v-row>
+    </v-container>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
-
-export default {
-  name: 'App',
-
-  components: {
-    HelloWorld,
-  },
-
-  data: () => ({
-    //
-  }),
-};
+    // AIzaSyC7oSfvfCBPvIKNfN5qflrA-28KQHaSgKQ
+    export default {
+        name: 'App',
+    };
 </script>
