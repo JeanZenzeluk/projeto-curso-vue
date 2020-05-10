736f8e708ef22fdb6104e5471fa987287673944a
jean zenzeluk <jeanzenzeluk@hotmail.com>
2020-05-10 17:30:22 -0300

    testee agora ok

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,59 @@
 <template>
-  <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
+    <v-app>
+        <p>testee</p>
+        <v-row align="center">
+            <v-col class="text-center" cols="12" sm="4">
+            <div class="my-2">
+                <v-btn text small>Normal</v-btn>
+            </div>
+            <div class="my-2">
+                <v-btn text small color="primary">Primary</v-btn>
+            </div>
+            <div class="my-2">
+                <v-btn text small color="error">Error</v-btn>
+            </div>
+            <div class="my-2">
+                <v-btn text small disabled>Disabled</v-btn>
+            </div>
+            </v-col>
 
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
+            <v-col class="text-center" cols="12" sm="4">
+            <div class="my-2">
+                <v-btn text>Normal</v-btn>
+            </div>
+            <div class="my-2">
+                <v-btn text color="primary">Primary</v-btn>
+            </div>
+            <div class="my-2">
+                <v-btn text color="error">Error</v-btn>
+            </div>
+            <div class="my-2">
+                <v-btn text disabled>Disabled</v-btn>
+            </div>
+            </v-col>
 
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar>
-
-    <v-content>
-      <HelloWorld/>
-    </v-content>
-  </v-app>
+            <v-col class="text-center" cols="12" sm="4">
+            <div class="my-2">
+                <v-btn text large>Normal</v-btn>
+            </div>
+            <div class="my-2">
+                <v-btn text large color="primary">Primary</v-btn>
+            </div>
+            <div class="my-2">
+                <v-btn text large color="error">Error</v-btn>
+            </div>
+            <div class="my-2">
+                <v-btn text large disabled>Disabled</v-btn>
+            </div>
+            </v-col>
+        </v-row>
+        <p>testeee 2</p>
+    </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
-
-export default {
-  name: 'App',
-
-  components: {
-    HelloWorld,
-  },
-
-  data: () => ({
-    //
-  }),
-};
+    // AIzaSyC7oSfvfCBPvIKNfN5qflrA-28KQHaSgKQ
+    export default {
+        name: 'App',
+    };
 </script>
