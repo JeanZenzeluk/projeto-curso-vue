741e9dad2358c90a4fd55822b7514a25516dd630
jean zenzeluk <jeanzenzeluk@hotmail.com>
2020-05-24 11:05:08 -0300

    funcionando com aula 49

--- src/App.vue
+++ src/App.vue
@@ -1,41 +1,16 @@
 <template>
     <v-app>
-        <p>testee</p>
-         <v-container>
-    <v-row no-gutters>
-      <v-col cols="12" sm="4" md="3">
-        <v-card
-          class="pa-2 red"
-          outlined
-          tile>
-          One of three columns
-        </v-card>
-      </v-col>
-      <v-col cols="12" sm="4" md="3">
-        <v-card
-          class="pa-2 green"
-          outlined
-          tile>
-          One of three columns
-        </v-card>
-      </v-col>
-      <v-col cols="12" sm="4" md="1">
-        <v-card
-          class="pa-2 blue"
-          outlined
-          tile>
-          One of three columns
-        </v-card>
-      </v-col>
-    </v-row>
-  </v-container>
-        <p>testeee 2</p>
+        <login-page />
     </v-app>
 </template>
 
 <script>
+
+    import LoginPage from './components/login/loginPage.vue';
+
     // AIzaSyC7oSfvfCBPvIKNfN5qflrA-28KQHaSgKQ
     export default {
         name: 'App',
+        components: { LoginPage },
     };
 </script>
---
+++ src/components/login/loginPage.vue
@@ -0,0 +1,40 @@
+<template>
+    <div>
+        <v-container fluid>
+            <v-row justify="center">
+                <v-col cols="12" md="3" class="text-center">
+                    <h6 class="headline">Login</h6>
+                    <v-text-field label="Token" v-model="token"/>
+                    <v-btn depressed dark color="green" block @click="login">
+                        Login
+                    </v-btn>
+                    <v-btn text @click="loginAsGuest">
+                        Entrar como visitante
+                    </v-btn>
+                    
+                </v-col>
+            </v-row>
+
+        </v-container>
+    </div>
+</template>
+
+<script>
+    // AIzaSyC7oSfvfCBPvIKNfN5qflrA-28KQHaSgKQ
+    export default {
+        name: 'loginPage',
+        data() {
+            return {
+                token: '',
+            }
+        },
+        methods: {
+            login(){
+                alert(this.token);
+            }, 
+            loginAsGuest(){
+                // TODO
+            }
+        }
+    };
+</script>
